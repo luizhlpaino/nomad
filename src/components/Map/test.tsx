82e1b19e54d03b582043aba707a229752023d81a
luizhlpaino <61785785+luizhlpaino@users.noreply.github.com>
2021-09-06 21:32:19 -0300

    added leaflet dependency and added map component

---
+++ src/components/Map/test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+import Map from '.'
+
+describe('<Map/>', () => {
+  it('should render without any marker', () => {
+    render(<Map />)
+
+    expect(
+      screen.getByRole('link', {
+        name: /a js library for interactive maps/i
+      })
+    )
+  })
+  it('should render with the marker in the correct place', () => {
+    const place = {
+      id: '1',
+      name: 'Petrópolis',
+      slug: 'petropolis',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+
+    const second_place = {
+      id: '2',
+      name: 'Reykjavik',
+      slug: 'reykjavik',
+      location: {
+        latitude: 129,
+        longitude: -50
+      }
+    }
+
+    render(<Map places={[place, second_place]} />)
+
+    expect(screen.getByTitle(/petrópolis/i)).toBeInTheDocument()
+    expect(screen.getByTitle(/reykjavik/i)).toBeInTheDocument()
+  })
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,20 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - BoilerPlate</title>
+        <title>Nomad</title>
         <link rel="shortcut icon" href="/img/icon-512.png"></link>
         <link rel="apple-touch-icon" href="/img/icon-512.png"></link>
         <link rel="manifest" href="/manifest.json"></link>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project made with React, NextJS and Styled Components"
+          content="A simple project to show my favorite places in the world."
         />
       </Head>
       <GlobalStyles />
