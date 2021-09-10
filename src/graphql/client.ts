0cdb3a748049335e60d490b03c594066db7e04e2
luizhlpaino <61785785+luizhlpaino@users.noreply.github.com>
2021-09-10 20:28:55 -0300

    added dynamic page generation

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,8 +1,7 @@
 import { GraphQLClient } from 'graphql-request'
 
 const endpoint = process.env.GRAPHQL_HOST
-console.log(`GRAPHQL_HOST = ${process.env.GRAPHQL_HOST}`)
-console.log(`GRAPHQL_TOKEN = ${process.env.GRAPHQL_TOKEN}`)
+
 const client = new GraphQLClient(endpoint, {
   headers: {
     authorization: `Bearer ${process.env.GRAPHQL_TOKEN}`
